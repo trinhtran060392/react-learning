4291d81e61f2d47476a52b36b453654c6ee12764
U-framgia0129-pc\framgia <framgia0129-pc+framgia@framgia0129-pc.framgia.com>
2016-06-03 14:24:31 -0700

    done studying basic term

--- public/app/react/tutorial3.js
+++ public/app/react/tutorial3.js
@@ -1,37 +1,138 @@
+// tutorial8.js
+
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        {commentNodes}
       </div>
     );
   }
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+
+    this.setState({text: e.target.value});
+  },
+   handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: send request to the server
+    this.props.onCommentSubmit(this.state);
+    this.setState({author: '', text: ''});
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+       <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post"/>
+      </form>
     );
   }
 });
 
-var CommentBox = React.createClass({
-	render: function () {
 
+var Comment = React.createClass({
+
+  rawMarkup: function () {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup}
+  },
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+      </div>
+    );
+  }
+})
+
+var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {
+      data: []
+    }
+  },
+  load: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({data: data});
+      }
+      .bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    var data = this.state.data;
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        var comments = data.concat([comment]);;
+        this.setState({data: commments});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function () {
+    this.load();
+    setInterval(this.load, this.props.pollInterval);
+  },
+  render: function () {
     return (
       <div className="commentBox">
       <h1>Comments</h1>
-      <CommentList/>
-      <CommentForm/>
+      <CommentList data={this.state.data}/>
+      <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
     ); 
   }
 })
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox url="api/comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
